5cd610aaad8d553cc2ed274276c03bec4f404fe3
sergey-samoylov <linews@yandex.ru>
2024-03-07 11:12:13 +0300

    GIT3inONEscript Thu Mar  7 11:12:13 AM MSK 2024

---
+++ django.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# CREATES A DJANGO PROJECT WITH FIRST APP OF YOUR CHOICE
+# author: Sergey Samoylov
+# name of the script: django.sh
+
+if [ "$#" -lt 2 ] # if args less than two
+    then
+        echo "Two names needed"
+        echo "Example: $0 project_name app_name"
+        exit 1 # exits with mistake code 1
+fi
+
+read -p "Enter directory name for your Django project: " main_project_folder
+project_name=$1
+app_name=$2
+
+mkdir ~/Dev/$main_project_folder
+cd ~/Dev/$main_project_folder
+
+python -m venv venv
+source venv/bin/activate
+
+pip install --upgrade pip
+pip install Django
+pip install flake8
+pip install black
+
+django-admin startproject $project_name
+cd $project_name
+python manage.py startapp $app_name
+
+# Modifying to settings.py
+cd $project_name
+app_name_caps=$(echo ${app_name^}Config)
+register_app="'$app_name.apps.$app_name_caps',"
+sed -i "/INSTALLED_APPS/a $register_app" settings.py
+sed -i '/from/,$!d' settings.py
+black settings.py
+
+cd ..
+python manage.py makemigrations && python manage.py migrate
+python manage.py runserver
