d511ea936138b3697eb90d39821309db3824350b
sergey-samoylov <linews@yandex.ru>
2024-03-29 23:28:05 +0300

    GIT3inONEscript Fri Mar 29 11:28:05 PM MSK 2024

--- django.sh
+++ django.sh
@@ -22,7 +22,7 @@ source venv/bin/activate
 
 echo "By default pip will be upgraded"
 echo "and following packages will be installed:"
-default_packages="django flake8 black"
+default_packages="django flake8 black python-dotenv"
 echo $default_packages
 read -p "Add another packages separated by space: " added_packages
 
@@ -39,17 +39,56 @@ django-admin startproject $project_name
 cd $project_name
 python manage.py startapp $app_name
 
-# Modifying to settings.py
+# Modifying settings.py
 cd $project_name
-app_name_caps=$(echo ${app_name^}Config)
-register_app="'$app_name.apps.$app_name_caps',"
+
+## regesters first app
+app_name_title=$(echo ${app_name^}Config)
+register_app="'$app_name.apps.$app_name_title',"
 sed -i "/INSTALLED_APPS/a $register_app" settings.py
+
+## looks for first "from" and deletes everything above
 sed -i '/from/,$!d' settings.py
+sed -i '/from django.contrib import admin/,$!d' urls.py
+
+
+## insert "load_dotenv()" on 3rd line
+sed -i '1 i from dotenv import load_dotenv' settings.py
+sed -i '4 a load_dotenv()' settings.py
+
+## insert "import os" and "Enter" before the 1st line
+sed -i '1 i import os\n' settings.py
+
+## place original SECRET_KEY into .env file (with removed spaces btw :wink )
+secret_key=$(grep "SECRET_KEY" settings.py) 
+echo $secret_key | sed 's/\s*//g' >> ../../.env
+not_secret_key="SECRET_KEY = os.getenv('SECRET_KEY')"
+
+## substitute SECRET_KEY with .env value
+sed -i "s/SECRET_KEY.*/$not_secret_key/g" settings.py
+
+## adding STATIC and MEDIA to settings.py
+static_dirs="STATICFILES_DIRS = ['static/']"
+media_url="MEDIA_URL = 'media/'"
+media_root="MEDIA_ROOT = os.path.join(BASE_DIR, 'media')"
+## have to do them in reverse order, so that they stack over one another
+sed -i "/STATIC_URL/a $media_root" settings.py
+sed -i "/STATIC_URL/a $media_url" settings.py
+sed -i "/STATIC_URL/a $static_dirs\n" settings.py
+
+## clean all comments
+sed -i "s/#.*//g" settings.py
+
+## clean up settings.py in installed apps
 black settings.py
 
 cd ..
+mkdir media static
 cd $app_name
 mkdir templates
+touch urls.py
+rm tests.py # comment this line, if tests needed in your project
 cd ..
 python manage.py makemigrations && python manage.py migrate
+echo "Don't forget to 'python manage.py createsuperuser'"
 python manage.py runserver
